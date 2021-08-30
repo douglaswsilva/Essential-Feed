52fa04124f2774dc7902792d169bc132747b32cf
Douglas Silva <silvadouglasw@gmail.com>
2021-08-30 12:21:50 -0300

    Create the feed item model.

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Douglas Silva on 30/08/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
