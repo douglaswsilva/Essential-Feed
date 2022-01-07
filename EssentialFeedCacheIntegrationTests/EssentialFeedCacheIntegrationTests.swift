df3482622105df165f9e2e24cf890017f5413668
Douglas Silva <silvadouglasw@gmail.com>
2022-01-07 12:57:28 -0300

    Add empty CoreData feed store specs

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -6,7 +6,29 @@
 //
 
 import XCTest
+import EssentialFeed
 
-class EssentialFeedCacheIntegrationTests: XCTestCase {
-
-}
+//class EssentialFeedCacheIntegrationTests: XCTestCase {
+//
+//    func test_load_deliversNoItemsOnEmptyCache() {
+//        let sut = makeSUT()
+//        
+//        let exp = expectation(description: "Wait for load completion")
+//        sut.load { result in
+//            switch result {
+//            case let .success(imageFeed):
+//                XCTAssertEqual(imageFeed, [], "Expect empty feed")
+//                
+//            case let .failure(error):
+//                XCTFail("Expected successful feed result, got \(error) instead")
+//            }
+//            
+//            exp.fulfill()
+//        }
+//        
+//        wait(for: [exp], timeout: 1.0)
+//    }
+//    
+//    // MARK: - Helpers
+//    private func
+//}
