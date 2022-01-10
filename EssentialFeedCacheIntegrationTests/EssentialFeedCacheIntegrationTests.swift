849b41a527c92fdcbfe81f722a80bd7658d1dbad
Douglas Silva <silvadouglasw@gmail.com>
2022-01-10 17:23:55 -0300

    Add `EssentialFeedCacheIntegrationTests` target to separate the pontially-slower cache integration tests from the fast unit/isolated tests.

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -2,33 +2,13 @@
 //  EssentialFeedCacheIntegrationTests.swift
 //  EssentialFeedCacheIntegrationTests
 //
-//  Created by Douglas Silva on 07/01/22.
+//  Created by Douglas Silva on 10/01/22.
+//  Copyright © 2022 Essential Developer. All rights reserved.
 //
 
 import XCTest
-import EssentialFeed
 
-//class EssentialFeedCacheIntegrationTests: XCTestCase {
-//
-//    func test_load_deliversNoItemsOnEmptyCache() {
-//        let sut = makeSUT()
-//        
-//        let exp = expectation(description: "Wait for load completion")
-//        sut.load { result in
-//            switch result {
-//            case let .success(imageFeed):
-//                XCTAssertEqual(imageFeed, [], "Expect empty feed")
-//                
-//            case let .failure(error):
-//                XCTFail("Expected successful feed result, got \(error) instead")
-//            }
-//            
-//            exp.fulfill()
-//        }
-//        
-//        wait(for: [exp], timeout: 1.0)
-//    }
-//    
-//    // MARK: - Helpers
-//    private func
-//}
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+
+}
